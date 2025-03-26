fbcfec54245bf9ca1fdf60351a0cd3fc931abf8d
Shabhareash <118547086+Shabhareash@users.noreply.github.com>
2025-03-26 19:34:03 +0530

    Delete LAB6 directory

--- LAB6/ques7.hs
+++
@@ -1,5 +0,0 @@
-stringlengthssum :: [String] -> Int
-stringlengthssum = foldl (+) 0 . map length
-
-main :: IO ()
-main = print (stringlengthssum ["hello", "world", "haskell"])
--- LAB6/ques9.hs
+++
@@ -1,5 +0,0 @@
-productofoddsquares :: [Int] -> Int
-productofoddsquares = foldl (*) 1 . map (^2) . filter odd
-
-main :: IO ()
-main = print (productofoddsquares [1, 2, 3, 4, 5, 6])
